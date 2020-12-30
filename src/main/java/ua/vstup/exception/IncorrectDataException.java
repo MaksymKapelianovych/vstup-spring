f61529689b291a0a1c09bdcafa237547501a0b1f
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2020-12-30 02:32:33 +0200

    Added exception for incorrect data

---
+++ src/main/java/ua/vstup/exception/IncorrectDataException.java
@@ -0,0 +1,9 @@
+package ua.vstup.exception;
+
+/**
+ * {@code IncorrectDataException} is exception that indicates that there was a problem with incoming data
+ */
+public class IncorrectDataException extends RuntimeException {
+    public IncorrectDataException(String s) { super(s);}
+}
+

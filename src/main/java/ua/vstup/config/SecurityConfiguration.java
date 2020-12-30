844bd4d91a60779f4913d53fa85dd1ab10950abf
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2020-12-30 02:32:46 +0200

    Added config classes

---
+++ src/main/java/ua/vstup/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package ua.vstup.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import ua.vstup.domain.Role;
+import ua.vstup.service.EntrantService;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final EntrantService provider;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/static/**", "/login-page", "/register-page").permitAll()
+                .antMatchers("/entrant").hasRole(Role.USER.name())
+                .antMatchers("/admin").hasRole(Role.ADMIN.name())
+
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .loginProcessingUrl("/login-page")
+                .usernameParameter("email")
+                .passwordParameter("pass")
+                .successForwardUrl("/login").permitAll()
+
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .permitAll()
+
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/static/**");
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(provider);
+    }
+}

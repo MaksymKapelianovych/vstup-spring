62c9c5f27d1299a53dd2f8474f3b8e6977f52bc1
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2021-01-06 00:27:01 +0200

    Changed parameters

--- src/main/java/ua/vstup/repository/RequestRepository.java
+++ src/main/java/ua/vstup/repository/RequestRepository.java
@@ -4,20 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ua.vstup.entity.RequestEntity;
-import ua.vstup.entity.RequestStateEntity;
+import ua.vstup.entity.*;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RequestRepository extends JpaRepository<RequestEntity, Integer> {
-    List<RequestEntity> findAllByEntrantId(Integer entrantId);
+    List<RequestEntity> findAllByEntrantEntity(EntrantEntity entrantEntity);
 
     List<RequestEntity> findAll();
 
-    List<RequestEntity> findAllByStatementId(Integer id);
+    List<RequestEntity> findAllByStatementEntity(StatementEntity statementEntity);
 
-    Optional<RequestEntity> findByEntrantIdAndFacultyId(Integer entrantId, Integer facultyId);
+    Optional<RequestEntity> findByEntrantEntityAndFacultyEntity(EntrantEntity entrantEntity, FacultyEntity facultyEntity);
 
     @Modifying
     @Query("UPDATE RequestEntity SET requestStateEntity = :state WHERE id = :id")

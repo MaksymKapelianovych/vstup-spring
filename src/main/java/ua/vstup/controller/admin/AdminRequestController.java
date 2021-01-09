633d515eb17f6915c3060c0a5a1052b17a80002a
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2021-01-09 18:13:24 +0200

    Added accept and disable methods

--- src/main/java/ua/vstup/controller/admin/AdminRequestController.java
+++ src/main/java/ua/vstup/controller/admin/AdminRequestController.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.vstup.domain.RequestState;
 import ua.vstup.service.RequestService;
 
 @Controller
-@RequestMapping("admin/request")
+@RequestMapping("/admin/request")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class AdminRequestController {
     private final RequestService requestService;
@@ -20,5 +22,16 @@ public class AdminRequestController {
         return "admin/request/request";
     }
 
+    @GetMapping("accept/{id}")
+    public String accept(@PathVariable("id") Integer id){
+        requestService.updateStateById(id, RequestState.ACCEPTED);
+        return "redirect:/admin/request";
+    }
+
+    @GetMapping("disable/{id}")
+    public String disable(@PathVariable("id") Integer id){
+        requestService.updateStateById(id, RequestState.DISABLED);
+        return "redirect:/admin/request";
+    }
 
 }
